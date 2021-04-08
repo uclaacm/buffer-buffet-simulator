c674c2de6e8eab746c69700f2af085a0088987fa
unknown <archisha.datta@gmail.com>
2021-04-07 21:44:01 -0700

    ready for functionality, missing some styling

--- src/components/VM_Instance.js
+++ src/components/VM_Instance.js
@@ -2,15 +2,12 @@ import React, { useState } from "react"
 import {setRegister, interpretCommand} from "../helperFunctions/VM_Helper"
 
 import info from "./info.svg"
-// import continue from "./continue.svg"
-// import next from "./next.svg"
-// import run from "./run.svg"
-// import stop from "./stop.svg"
+import cont from "./continue.svg"
+import next from "./next.svg"
+import run from "./run.svg"
+import stop from "./stop.svg"
+import close from "./close.svg"
 
-
-
-// import { MdPlayArrow } from "react-icons/md";
-{/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link> */}
 const VM_Instance = () => {
     //specify the initial memory array buffer (size in bytes)
     const STACK_SIZE = 256
@@ -19,96 +16,243 @@ const VM_Instance = () => {
     setRegister('%edi', 40, memoryDV)
     memoryDV.setUint32(40, 134)
 
-
+    class Toolbar extends React.Component{
+        constructor(props){
+            super(props);
+            this.state = {
+                run: true,
+                rest: false,
+            }
+            this.handleButton = this.handleButton.bind(this)
+        }
+    
+        handleButton(event){
+            var val =  event.target.id;
+            console.log(val);
+            console.log(this.state);
+            if(run){
+                this.setState({run: false, rest: true});
+            }
+            if(val === 'stop'){
+                console.log("im pressing stop");
+                this.setState({run: true, rest: false});
+            }
+            // console.log(this.state);
+        }
+        render(){
+            return(
+                <div className = "toolbar grid-container">
+                    <h2>Toolbar</h2>
+                    <button class="toolButton" disabled={!this.state.run} onClick={this.handleButton} id = "run"><img className = "btn" src = {run}></img> Run</button>
+                    <button class="toolButton" disabled={!this.state.rest} onClick={this.handleButton} id = "stop"><img className = "btn" src = {stop}></img> Stop</button>
+                    <button class="toolButton" disabled={!this.state.rest} onClick={this.handleButton} id = "next"><img className = "btn" src = {next}></img> Next</button>
+                    <button class="toolButton" disabled={!this.state.rest} onClick={this.handleButton} id = "cont"><img className = "btn" src = {cont}></img> Continue</button>
+                </div>
+            );
+        }
+    }
 
     class Dropdown extends React.Component {
         constructor(props) {
           super(props);
-          this.state = {value: ''};
-      
           this.handleSubmit = this.handleSubmit.bind(this);
+          this.state = {options: [
+            <option value="" selected disabled hidden>&nbsp;&nbsp;&nbsp;Select Program</option>,
+            <option value="Sum">&nbsp;&nbsp;&nbsp;Sum</option>,
+            <option value="If-Else">&nbsp;&nbsp;&nbsp;If-Else</option>,
+            <option value="Switch Statement">&nbsp;&nbsp;&nbsp;Switch Statement</option>,
+            <option value="For Loop">&nbsp;&nbsp;&nbsp;For Loop</option>,
+            <option value="Recursion">&nbsp;&nbsp;&nbsp;Recursion</option>,
+          ]}
+
         }
            
         handleSubmit(event) {
             var val = event.target.value;
-            this.setState({value: val});
             console.log('A program was submitted: ' + val);
             // event.preventDefault();
-            if (val === 'Hello World') {
+            if (val === 'Sum') {
                 setSourceCode(
                     <p>
-                        int main()
+                        int addNum(int a, int b)
                         <br/>&#x0007B;
-                        <br/>&emsp;&emsp;printf("Hello World");
-                        <br/>&emsp;&emsp;return 0;
+                        <br/>&emsp;&emsp;int sum = a + b;
+                        <br/>&emsp;&emsp;return sum;
                         <br/>&#x0007D;
-                    </p>
+                    </p>                    
+                )
+                this.setState({options: 'gross'});
+                console.log(this.state.options);
+                setAssemblyCode(
+                    <p>
+                        0000000000400502 &lt;addNum&gt;:
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                     </p>
                 )
             }
             else if (val === 'For Loop'){
                 setSourceCode(
                     <p>
-                        for loop
+                        int incrementAdd(int a)
+                        <br/>&#x0007B;
+                        <br/>&emsp;&emsp;int sum = 0;
+                        <br/>&emsp;&emsp;for (int i = 0; i &#60; a&#44; i++) &#x0007B;
+                        <br/>&emsp;&emsp;&emsp;&emsp;sum += i;
+                        <br/>&emsp;&emsp;&#x0007D;
+                        <br/>&emsp;&emsp;return sum;
+                        <br/>&#x0007D;
                     </p>
+                )
+                setAssemblyCode(
+                    <p>
+                        0000000000400502 &lt;incrementAdd&gt;:
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                     </p>
                 )
             }
             else if (val === 'If-Else'){
                 setSourceCode(
                     <p>
-                        if-else
+                        int getMax(int a, int b)
+                        <br/>&#x0007B;
+                        <br/>&emsp;&emsp;if (b &#62; a)
+                        <br/>&emsp;&emsp;&emsp;&emsp;return b;
+                        <br/>&emsp;&emsp;else
+                        <br/>&emsp;&emsp;&emsp;&emsp;return a;
+                        <br/>&#x0007D;
                     </p>
+                )
+                setAssemblyCode(
+                    <p>
+                        0000000000400502 &lt;getMax&gt;:
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                     </p>
+                )
+            }
+            else if (val === 'Switch Statement'){
+                setSourceCode(
+                    <p>
+                        int switchTable(int n)
+                        <br/>&#x0007B;
+                        <br/>&emsp;&emsp;int sum = 3;
+                        <br/>&emsp;&emsp;switch (n) &#x0007B;
+                            <br/>&emsp;&emsp;&emsp;&emsp;case 1: 
+                            <br/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;sum += n;
+                            <br/>&emsp;&emsp;&emsp;&emsp;case 2: 
+                            <br/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;sum -= 2;
+                            <br/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;break;
+                            <br/>&emsp;&emsp;&emsp;&emsp;case 3:
+                            <br/>&emsp;&emsp;&emsp;&emsp;case 4:
+                            <br/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;sum = 34 + n;
+                            <br/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;break;
+                            <br/>&emsp;&emsp;&emsp;&emsp;default:
+                            <br/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;sum = 0;
+                            <br/>&emsp;&emsp;&#x0007D;
+                            <br/>&emsp;&emsp;return sum;
+                        <br/>&#x0007D;
+                    </p>
+                )
+                setAssemblyCode(
+                    <p>
+                        0000000000400502 &lt;switchTable&gt;:
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                     </p>
+                )
+            }
+            else if (val === 'Recursion'){
+                setSourceCode(
+                    <p>
+                        int incrementAdd(int a)
+                        <br/>&#x0007B;
+                        <br/>&emsp;&emsp;if (n == 1)
+                        <br/>&emsp;&emsp;&emsp;&emsp;return 1;
+                        <br/>
+                        <br/>&emsp;&emsp;return n * factorial(n-1);
+                        <br/>&#x0007D;
+                    </p>
+                )
+                setAssemblyCode(
+                    <p>
+                        0000000000400502 &lt;incrementAdd&gt;:
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                        <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+                     </p>
                 )
             }
         }
       
         render() {
           return (
-            // <form onSubmit={this.handleSubmit}>
-            //   <label>
-            //     Name:
-            //     <input type="text" value={this.state.value} onChange={this.handleChange} />
-            //   </label>
-            //   <input type="submit" value="Submit" />
-            // </form>
             <select className = "program" onChange={this.handleSubmit}>
-                <option value="" selected disabled hidden>&nbsp;&nbsp;&nbsp;Select Program</option>
-                <option value="Hello World">&nbsp;&nbsp;&nbsp;Hello World</option>
-                <option value="For Loop">&nbsp;&nbsp;&nbsp;For Loop</option>
+                {/* <option value="" selected disabled hidden>&nbsp;&nbsp;&nbsp;Select Program</option>
+                <option value="Sum">&nbsp;&nbsp;&nbsp;Sum</option>
                 <option value="If-Else">&nbsp;&nbsp;&nbsp;If-Else</option>
+                <option value="Switch Statement">&nbsp;&nbsp;&nbsp;Switch Statement</option>
+                <option value="For Loop">&nbsp;&nbsp;&nbsp;For Loop</option>
+                <option value="Recursion">&nbsp;&nbsp;&nbsp;Recursion</option> */}
+                {this.state.options}
             </select>
           );
         }
       }
 
-
-
-
-
-    // function changeDropdown (props){
-    //     if(props === "Hello World"){
-    //         return(
-    //             <div> Hello World</div>
-    //         );
-    //     }
-    //     else if (props === "For Loop"){
-    //         return(
-    //             <div>For loop</div>
-    //         );
-    //     }
-    //     else if (props === "If-Else"){
-    //         return(
-    //             <div>If-Else</div>
-    //         );
-    //     }
-    // }
-
     const [sourceCode, setSourceCode] = useState(
         <p>
-            int main()
+            int addNum(int a, int b)
             <br/>&#x0007B;
-            <br/>&emsp;&emsp;printf("Hello World");
-            <br/>&emsp;&emsp;return 0;
+            <br/>&emsp;&emsp;int sum = a + b;
+            <br/>&emsp;&emsp;return sum;
             <br/>&#x0007D;
+        </p>
+    );
+
+    function Row (props) {
+        return (
+          <div className = "row">
+              <label class = "container">
+                  <input type="checkbox" id="1"></input>
+                  <span class="checkmark"></span>
+              </label>
+            <div>
+                &emsp;&emsp;{props.address}:&emsp;&emsp;&emsp;&emsp;{props.content}&emsp;&emsp;&emsp;&emsp;{props.command}&emsp;&emsp;{props.parameters}
+            </div>
+          </div>
+        )
+      }
+
+    const [assemblyCode, setAssemblyCode] = useState(
+        <p>
+            0000000000400502 &lt;main&gt;:
+            <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+            <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+            <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+            <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+            <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
+            <Row address = "400502" content = "b4 a4 05 40 00" command = "mov" parameters = "$0x4005a4, %edi"></Row>
         </p>
     );
 
@@ -158,6 +302,17 @@ const VM_Instance = () => {
                 <li>r13D: </li>
             </ul>
             */}
+
+            {/* modal */}
+            <div className = "modal"></div>
+            <div className = "popupContainer">
+                <div className = "popup">
+                    <img className = "close" src = {close}></img>
+                    <h2 className = "instructions">Instructions</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu. Lacinia quis vel eros donec ac odio tempor orci dapibus. </p>
+                </div>
+            </div>
+
             <div className = "all grid-container">
                 <div className = "stack">
                     <h2>Stack</h2>
@@ -189,31 +344,18 @@ const VM_Instance = () => {
                 <div>
                 <div className = "topNav grid-container">
                     <h1>Buffer Buffet</h1>
-                    <img className = "info" src = {info}></img>
+                    <img className = "info" src = {info} onClick ={() => {
+                        console.log("info");
+                    }}></img>
                 </div>
 
                 <div className = "content grid-container">
                     <div className = "section">
                     <div className = "header grid-container">
                             <h2>C</h2>
-                            {/* <select className = "program" onChange={
-                                () => setSourceCode(changeDropdown(document.getElementById("program").value))
-                            }>
-                                <option value="" selected disabled hidden>&nbsp;&nbsp;&nbsp;Select Program</option>
-                                <option value="Hello World">&nbsp;&nbsp;&nbsp;Hello World</option>
-                                <option value="For Loop">&nbsp;&nbsp;&nbsp;For Loop</option>
-                                <option value="If-Else">&nbsp;&nbsp;&nbsp;If-Else</option>
-                            </select> */}
                             <Dropdown></Dropdown>
                         </div>
-                        <div className="code">
-                            {/* <p>
-                                int main()
-                                <br/>&#x0007B;
-                                <br/>&emsp;&emsp;printf("Hello World");
-                                <br/>&emsp;&emsp;return 0;
-                                <br/>&#x0007D;
-                            </p> */}
+                        <div className="code source">
                             {sourceCode}
                         </div>
                     </div>
@@ -223,18 +365,18 @@ const VM_Instance = () => {
                         </div>
                         
                         <div className="code">
-                            <p>0000000000400502 &lt;main&gt;:</p>
-                            {/* <img className = "temp" src = {temp}></img> */}
+                            {assemblyCode}
                         </div>
                     </div>
                 </div>
-                    <div className = "toolbar grid-container">
+                    {/* <div className = "toolbar grid-container">
                         <h2>Toolbar</h2>
-                        <button class="toolButton"><img className = "btn" src = {info}></img> Run</button>
-                        <button class="toolButton"><img className = "btn" src = {info}></img> Stop</button>
-                        <button class="toolButton"><img className = "btn" src = {info}></img> Next</button>
-                        <button class="toolButton"><img className = "btn" src = {info}></img> Continue</button>
-                    </div>
+                        <button class="toolButton"><img className = "btn" src = {run}></img> Run</button>
+                        <button class="toolButton"><img className = "btn" src = {stop}></img> Stop</button>
+                        <button class="toolButton"><img className = "btn" src = {next}></img> Next</button>
+                        <button class="toolButton"><img className = "btn" src = {cont}></img> Continue</button>
+                    </div> */}
+                    <Toolbar></Toolbar>
                 </div>
                 
             </div>
